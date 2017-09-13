adb8a36b9d62e857f73ad2a140e2a41ebafd51f0
Charlie Kelson and Kaili Nishihira <charlie@>
2017-09-13 15:18:27 -0700

    initial commit.

--- js/object_name-interface.js
+++ js/object_name-interface.js
@@ -1,13 +1,30 @@
-import { ObjectName } from './../js/object_name.js';
 
-$(function() {
-  $('#name-of-form').submit(function(event) {
+$(document).ready(function() {
+  $('#bike-form').submit(function() {
     event.preventDefault();
-    var nameOfField = $('#field-id').val();
-    var newObject = new ObjectName(propertyInput);
-    var output = newObject.methodName(InputParameter);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
+    let manufacturer = $('#manufacturer').val();
+    let distance = $('#distance').val();
+
+    $('#manufacturer').val("");
+    $('#distance').val("");
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${manufacturer}&location=IP&distance=${distance}&stolenness=stolen`).then(function(response) {
+        // $('#solution').text(`${Object.values(response)[0]}`);
+        let bikeArray = response.bikes;
+        $('#solution').text("");
+        for(let i = 0; i <= bikeArray.length; i++) {
+        $('#solution').append("<li>" + "Title: " + response.bikes[i].title + "</li>");
+        $('#solution').append("<li>" + "Serial: " + response.bikes[i].serial + "</li>");
+        $('#solution').append("<li>" + "Manufacturer: " + response.bikes[i].manufacturer_name + "</li>");
+        $('#solution').append("<li>" + "Frame Model: " + response.bikes[i].frame_model + "</li>" + "<br>");
+      }
+
+
+
+
+        console.log(response.bikes);
+
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
 });
